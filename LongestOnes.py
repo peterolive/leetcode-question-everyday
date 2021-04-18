0d12c240d9b9d87064811a81c02b2667a8668c37
xyang425 <xyang425@gatech.edu>
2021-04-18 11:36:58 -0400

    1004. Max Consecutive Ones III

---
+++ LongestOnes.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = 0
+
+        for right in range(len(nums)):
+            k -= 1 - nums[right]
+            if k < 0:
+                k += 1 - nums[left]
+                left += 1
+        return right - left + 1
