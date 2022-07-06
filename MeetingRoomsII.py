616ea4768e1bee1c2716416274f2687944b9e4c0
Peter Yang <pyang@hudson-trading.com>
2022-07-06 18:07:01 -0400

    920 · Meeting Rooms; 919 · Meeting Rooms II

---
+++ MeetingRoomsII.py
@@ -0,0 +1,31 @@
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def minMeetingRooms(self, intervals):
+        # Write your code here
+        if len(intervals) == 0:
+            return 0
+        
+        intervals = sorted(intervals, key=lambda x: x.start)
+        import heapq
+        heap = []
+        heapq.heappush(heap, intervals[0].end)
+        
+        for i in range(1, len(intervals)):
+            if intervals[i].start < heap[0]:
+                heapq.heappush(heap, intervals[i].end)
+            else:
+                heapq.heappop(heap)
+                heapq.heappush(heap, intervals[i].end)
+
+        return len(heap)
