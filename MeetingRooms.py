ce69c077c4640eb6accb404d45cadb20d68ec51a
Peter Yang <pyang@hudson-trading.com>
2022-07-07 17:33:50 -0400

    919, 920, you cannot use heap[0] because heap is not sorted

--- MeetingRooms.py
+++ MeetingRooms.py
@@ -19,12 +19,12 @@ class Solution:
         intervals = sorted(intervals, key=lambda x: x.start)
         heap = []
         heapq.heappush(heap, intervals[0].end)
+        meeting_end = intervals[0].end
         
         for i in range(1, len(intervals)):
-            if intervals[i].start < heap[0]:
+            if intervals[i].start < meeting_end:
                 return False
             else:
-                heapq.heappop(heap)
-                heapq.heappush(heap, intervals[i].end)
+                meeting_end = intervals[i].end
                 
         return True
--- MeetingRoomsII.py
+++ MeetingRoomsII.py
@@ -1,5 +1,12 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    Interval,
+)
+
 """
-Definition of Interval.
+Definition of Interval:
 class Interval(object):
     def __init__(self, start, end):
         self.start = start
@@ -11,21 +18,22 @@ class Solution:
     @param intervals: an array of meeting time intervals
     @return: the minimum number of conference rooms required
     """
-    def minMeetingRooms(self, intervals):
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
         # Write your code here
-        if len(intervals) == 0:
-            return 0
-        
-        intervals = sorted(intervals, key=lambda x: x.start)
         import heapq
         heap = []
+
+        if not intervals:
+            return 0
+
+        intervals = sorted(intervals, key= lambda x: x.start)
+
         heapq.heappush(heap, intervals[0].end)
-        
+
         for i in range(1, len(intervals)):
-            if intervals[i].start < heap[0]:
-                heapq.heappush(heap, intervals[i].end)
-            else:
-                heapq.heappop(heap)
-                heapq.heappush(heap, intervals[i].end)
+            meeting_end = heapq.heappop(heap)
+            if meeting_end > intervals[i].start:
+                heapq.heappush(heap, meeting_end)
+            heapq.heappush(heap, intervals[i].end)
 
         return len(heap)
